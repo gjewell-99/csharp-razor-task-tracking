b4728c36792e73da411dd827601cf6202f43abbf
George Jewell <George.Jewell@insperity.com>
2022-03-31 14:19:00 -0500

    Module 6

--- Tasker/Pages/CreateTask.cshtml.cs
+++ Tasker/Pages/CreateTask.cshtml.cs
@@ -25,6 +25,11 @@ namespace Tasker.Pages
 
         public IActionResult OnPost()
         {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
             _context.Tasks.Add(NewTask);
             _context.SaveChanges();
 
